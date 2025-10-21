bee0b9b23b849e011a9e651cb476abdc539a1a45
ShilpaBK-CPU <shilpashettybk2025@outlook.com>
2025-10-21 14:01:39 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Call BigOfTwo
+        BigOfTwo.main(args);
+
+        // Call Factorial
+        Factorial.main(args);
+    }
+}
