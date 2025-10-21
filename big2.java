4a069d980028d09b8d328039348051080f3e95d6
ShilpaBK-CPU <shilpashettybk2025@outlook.com>
2025-10-21 13:33:54 +0530

    Create big2.java

---
+++ big2.java
@@ -0,0 +1,14 @@
+public class BigOfTwo {
+    public static void main(String[] args) {
+        int a = 7;       // Change to any value
+        int b = 89;      // Change to any value
+
+        if (a > b) {
+            System.out.println(a + " is bigger.");
+        } else if (b > a) {
+            System.out.println(b + " is bigger.");
+        } else {
+            System.out.println("Both numbers are equal.");
+        }
+    }
+}
